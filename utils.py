ffd06fc8e1aaf60f12e68427654fb36ede384e29
SuneTheFrack <sunefrack69@gmail.com>
2025-07-20 15:02:09 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,15 +5,14 @@ from datetime import datetime
 
 def get_datum_tid(data):
     now = datetime.now()
-    
-    # Datum: om saknas → dagens datum
+
     datum = data.get("datum")
-    if not datum or str(datum).lower() == "nu":
+    if not datum or str(datum).strip().lower() == "nu":
         datum = now.strftime("%Y-%m-%d")
 
-    # Tid: om saknas eller är "nu" → aktuell tid
     tid = data.get("tid")
-    if not tid or str(tid).lower() == "nu":
+    if not tid or str(tid).strip().lower() == "nu":
         tid = now.strftime("%H:%M")
 
     return datum, tid
+
