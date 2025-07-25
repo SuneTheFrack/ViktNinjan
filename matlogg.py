672d12ed1f0e546a21c2e0cba9477f57fa39d599
SuneTheFrack <sunefrack69@gmail.com>
2025-07-25 21:05:33 +0200

    Update matlogg.py

--- matlogg.py
+++ matlogg.py
@@ -1,20 +1,39 @@
 # matlogg.py
 # Tar emot måltidsdata och loggar det till fliken "Mat" i Google Sheets
 
-from flask import request, jsonify
+from flask import jsonify
 from utils.sheets import skriv_till_sheet
 from tidutils import get_datum_tid
 
 def logg_maltid_intern(data):
     datum, tid = get_datum_tid(data)
     person = data.get("person", "Henrik")
+    mal = data.get("mal", "")
     innehall = data.get("innehall", "")
     kcal = data.get("kcal", 0)
+    protein = data.get("protein", 0)
     fett = data.get("fett", 0)
+    mattat_fett = data.get("mattat_fett", 0)
+    kolhydrater = data.get("kolhydrater", 0)
+    salt = data.get("salt", 0)
+    fibrer = data.get("fibrer", 0)
     vatska = data.get("vatska_ml", 0)
 
-    # Anpassa rad efter dina kolumner i bladet "Mat"
-    rad = [datum, tid, person, innehall, kcal, fett, vatska]
+    rad = [
+        datum,
+        tid,
+        person,
+        mal,
+        innehall,
+        kcal,
+        protein,
+        fett,
+        mattat_fett,
+        kolhydrater,
+        salt,
+        fibrer,
+        vatska
+    ]
 
     skriv_till_sheet(rad, blad_namn="Mat")
 
