2dca876655d28fa31800540071da740e178313ee
SuneTheFrack <sunefrack69@gmail.com>
2025-08-02 18:15:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from routers import matlogg, viktlogg, rorelselogg, preferences
 app = FastAPI()
 
 app.include_router(matlogg.router)
-app.include_router(viktlogg.router)
-app.include_router(rorelselogg.router)
-app.include_router(preferenser.router)
+# app.include_router(viktlogg.router)
+# app.include_router(rorelselogg.router)
+# app.include_router(preferenser.router)
 
 @app.get("/")
 def root():
