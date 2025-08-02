dfcfc3e2773f9ca8a83990b994b97a08f297ac81
SuneTheFrack <sunefrack69@gmail.com>
2025-08-02 19:45:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ app.include_router(matlogg.router)
 @app.get("/")
 def root():
     return {"message": "ViktNinjan API är igång"}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # Default till 8000 om PORT inte finns
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
