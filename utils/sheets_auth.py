2875ebfcdc5766de0abc9c308665d1f604815088
SuneTheFrack <sunefrack69@gmail.com>
2025-08-02 19:25:24 +0200

    Update sheets_auth.py

--- utils/sheets_auth.py
+++ utils/sheets_auth.py
@@ -1,11 +1,14 @@
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-import json
 import os
+import json
+from google.oauth2 import service_account
+import gspread
 
 def get_sheets_service():
-    creds = service_account.Credentials.from_service_account_info(
-        json.loads(os.environ["SERVICE_ACCOUNT_JSON"]),
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
-    return build("sheets", "v4", credentials=creds)
+    service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
+    creds = service_account.Credentials.from_service_account_info(service_account_info)
+    gc = gspread.authorize(creds)
+    
+    sheet_id = os.environ["SHEET_ID"]
+    sh = gc.open_by_key(sheet_id)
+    
+    return sh, sheet_id  # OBS! returnera båda
